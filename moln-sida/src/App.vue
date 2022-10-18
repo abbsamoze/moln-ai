e863c4b5db1640f1f1e4b244d9702bf6970ecef7
abbsamoze <69853489+abbsamoze@users.noreply.github.com>
2022-10-18 15:33:35 +0200

    added project

---
+++ moln-sida/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  height: 667px;
+  width: 375px;
+  background-image: url("https://wallpaperaccess.com/full/896979.jpg");
+  background-size: cover;
+}
+</style>
